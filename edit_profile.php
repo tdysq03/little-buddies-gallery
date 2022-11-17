cbd9958a4408d730d95fe84f9f63f719b4cebeb4
ApichaTop <topzza9583@gmail.com>
2022-11-17 15:50:12 +0700

    Edit รวมไฟล์
    รวมไฟล์จ้า

--- edit_profile.php
+++ edit_profile.php
@@ -2,6 +2,37 @@
 session_start();
 include('server.php');
 @ini_set('display_errors', '0');
+session_start();
+    include('server.php');
+    $errors = array();
+    $nameTemp = $_SESSION['username'];
+    if (isset($_POST['submit'])) {
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $username = mysqli_real_escape_string($conn, $_POST['username']);
+        $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+        if (empty($username)) {
+            array_push($errors, "Username is required");
+            $_SESSION['error'] = "Username is required";
+        }
+        if (empty($email)) {
+            array_push($errors, "Email is required");
+            $_SESSION['error'] = "Email is required";
+        }
+        if (empty($password)) {
+            array_push($errors, "Password is required");
+            $_SESSION['error'] = "Password is required";
+        }
+
+        if (count($errors) == 0) {
+            $password = md5($password);
+            $sql = "UPDATE users SET  email = '$email', username = '$username', password = '$password' WHERE username='$nameTemp' ";
+            mysqli_query($conn, $sql);
+            header('location: logout.php');
+        } else {
+            header("location: edit_profile.php");
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -74,7 +105,7 @@ include('server.php');
         <h2 class="rounded-pill mt-3 mb-4" style="color: #C6C09c;">Edit Profile</h2><hr>
         <div class="row">
             <div class="col-sm-12">
-                <form action = "edit_db.php" method = "post">
+                <form action = "#" method = "post">
                 <?php if (isset($_SESSION['error'])) :?>
                             <div class="alert alert-warning alert-dismissible fade show round" role="alert">
                             <h4 class="alert-heading">Please try again</h4>
